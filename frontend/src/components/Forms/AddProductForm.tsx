e52b4d23baef46edd55a95280ab5c18d9e52f37c
shireenzd <shireenzeineddine@gmail.com>
2024-02-16 11:56:03 +0200

    edit styles

--- frontend/src/components/Forms/AddProductForm.tsx
+++ frontend/src/components/Forms/AddProductForm.tsx
@@ -15,7 +15,8 @@ function AddProductForm({ product, setProduct }: { product: any, setProduct: Fun
         backgroundColor: '#e5e5e5',
         opacity: '70%',
         padding: '20px',
-        boxShadow: ' 4px 4px 4px 4px rgba(0, 0, 0, 0.3)',
+        boxShadow: ' 4px 4px 4px 4px #c5c5c5',
+        // boxShadow: '3px 3px 5px #c5c5c5 ,  -3px -3px 5px #c5c5c5'
 
     }
 
@@ -49,15 +50,15 @@ function AddProductForm({ product, setProduct }: { product: any, setProduct: Fun
     return (
         <form className="flex flex-col justify-center gap-2 rounded-xl" style={formStyle}>
             <span className={inputRowStyle}>
-                <label htmlFor="product-name">product-name</label>
+                <label className='font-medium' htmlFor="product-name">product-name</label>
                 <input className='addProductinput' type="text" name="product-name" id="product-name" value={product.name} onChange={(e) => setProduct({ ...product, name: e.target.value })} />
             </span>
             <span className={inputRowStyle}>
-                <label htmlFor="product-price">product-price</label>
+                <label className='font-medium' htmlFor="product-price">product-price</label>
                 <input className='addProductinput' type="number" name="product-price" id="product-price" value={product.price} onChange={(e) => setProduct({ ...product, price: parseInt(e.target.value) })} />
             </span>
             <span className={inputRowStyle}>
-                <label htmlFor="product-category">product-category</label>
+                <label className='font-medium' htmlFor="product-category">product-category</label>
                 <select id="Category" onChange={(e) => setProduct({ ...product, category: e.target.value })} className='addProductinput border-[1px] border-gray-700 rounded-md w-full font-medium'>
                     <option value=''>Category</option>
                     <option value='furniture'>furniture</option>
@@ -68,16 +69,16 @@ function AddProductForm({ product, setProduct }: { product: any, setProduct: Fun
                 </select>
             </span>
             <span className={inputRowStyle}>
-                <label htmlFor="product-description">product-description</label>
+                <label className='font-medium' htmlFor="product-description">product-description</label>
                 <input type="text" className='addProductinput' name="product-description" id="product-description" value={product.description} onChange={(e) => setProduct({ ...product, description: e.target.value })} />
             </span>
             <span className={inputRowStyle}>
-                <label htmlFor="product-pictures">Pictures</label>
+                <label className='font-medium' htmlFor="product-pictures">Pictures</label>
                 <input className='addProductinput file:border-0 file:bg-transparent file:text-gray-600 file:text-sm file:font-medium flex h-10 w-full rounded-md border border-input bg-white text-sm text-gray-400'
                     onChange={(e) => e.target.files && setProduct({ ...product, pictures: Array.from(e.target.files) })} type="file" name="product-pictures" id="product-pictures" multiple={true} />
             </span>
             <span className={inputRowStyle}>
-                <button className='classButton' onClick={handleSubmit} type="button">Add Product</button>
+                <button className='classButton ' onClick={handleSubmit} type="button">Add Product</button>
             </span>
         </form>
     )
